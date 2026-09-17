85da4906e6da9af5cd61fd667bc77abd5c5bf069
AbdullahShublaq <abdsh@students.iugaza.edu.ps>
2026-09-17 23:08:07 +0300

    reverb config

--- config/reverb.php
+++ config/reverb.php
@@ -82,7 +82,7 @@ return [
                     'scheme' => env('REVERB_SCHEME', 'https'),
                     'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
                 ],
-                'allowed_origins' => ['*'],
+                'allowed_origins' => [env('REVERB_ALLOW_ORIGIN', '*')],
                 'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
                 'activity_timeout' => env('REVERB_APP_ACTIVITY_TIMEOUT', 30),
                 'max_connections' => env('REVERB_APP_MAX_CONNECTIONS'),
